72493296aeb045ab4f8dbe34b90cf15aafd563ed
admin <admin@ADMIN-PC>
2019-07-30 09:24:11 +0700

    hhh

--- DAL/LopHocDAL.cs
+++ DAL/LopHocDAL.cs
@@ -11,7 +11,7 @@ namespace DAL
     {
         public List<LopHoc> getAlllop()
         {
-            SqlDataReader reader = ReadData("select * fro333m Lop");
+            SqlDataReader reader = ReadData("select * from Lop");
             List<LopHoc> dsLop = new List<LopHoc>();
             while(reader.Read())
             {
